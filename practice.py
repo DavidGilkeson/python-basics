9d647d87872a8d09dd1aab1a991dc9c2825e233e
David Gilkeson <davidgilkeson8@gmail.com>
2021-01-07 01:13:35 +0000

    Added Strings

--- practice.py
+++ practice.py
@@ -1,5 +1,18 @@
-
+import random
 total = 0
 
-for num in range(10, -1, -1):
-  print(num)
+# for num in range(10, -1, -1):
+#   print(num)
+  
+  
+x =  random.randint(0,10) 
+
+print(x)
+
+def myFunction(name, age):
+  x = 5
+  for x in range(1, 10):
+    if x == 5:
+      print(f"hello {x} {name} {age}")
+      
+myFunction("David", 30)
