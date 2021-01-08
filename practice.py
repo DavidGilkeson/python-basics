99559014f943b9ec1ecee3de170afd2ba69a4e21
David Gilkeson <davidgilkeson8@gmail.com>
2021-01-08 01:50:34 +0000

    Booleans added

--- practice.py
+++ practice.py
@@ -1,18 +1,28 @@
+# Guess the number game
+
 import random
-total = 0
 
-# for num in range(10, -1, -1):
-#   print(num)
+print("Hello, What is your name")#
+
+name = input()
+
+secretNumber = random.randint(1,20)
+
+print(f"Well {name}, I am thinking of a number between 1 and 20.")
+
+# Guess 6 times
+
+for guessTaken in range(1, 7):
+  print("Take a guess")
+  guess = int(input())
+  if guess < secretNumber:
+    print("Your guess is too low.")
+  elif guess > secretNumber:
+    print("Your guess is too high")
+  else:
+    break # This condition is the right guess
   
-  
-x =  random.randint(0,10) 
-
-print(x)
-
-def myFunction(name, age):
-  x = 5
-  for x in range(1, 10):
-    if x == 5:
-      print(f"hello {x} {name} {age}")
-      
-myFunction("David", 30)
+if guess == secretNumber:
+  print(f"Good job {name}! You guessed your number in {guessTaken} guesses")
+else:
+  print(f"Nope. The number I was thinking of was {secretNumber}")
