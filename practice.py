56d3a46697cfe50f688aebba2ec8ae902388b928
David Gilkeson <davidgilkeson8@gmail.com>
2021-01-13 00:49:57 +0000

    Lists updated

--- practice.py
+++ practice.py
@@ -1,28 +1,35 @@
-# Guess the number game
+# # Guess the number game
 
-import random
+# import random
 
-print("Hello, What is your name")#
+# print("Hello, What is your name")#
 
-name = input()
+# name = input()
 
-secretNumber = random.randint(1,20)
+# secretNumber = random.randint(1,20)
 
-print(f"Well {name}, I am thinking of a number between 1 and 20.")
+# print(f"Well {name}, I am thinking of a number between 1 and 20.")
 
-# Guess 6 times
+# # Guess 6 times
 
-for guessTaken in range(1, 7):
-  print("Take a guess")
-  guess = int(input())
-  if guess < secretNumber:
-    print("Your guess is too low.")
-  elif guess > secretNumber:
-    print("Your guess is too high")
-  else:
-    break # This condition is the right guess
+# for guessTaken in range(1, 7):
+#   print("Take a guess")
+#   guess = int(input())
+#   if guess < secretNumber:
+#     print("Your guess is too low.")
+#   elif guess > secretNumber:
+#     print("Your guess is too high")
+#   else:
+#     break # This condition is the right guess
   
-if guess == secretNumber:
-  print(f"Good job {name}! You guessed your number in {guessTaken} guesses")
-else:
-  print(f"Nope. The number I was thinking of was {secretNumber}")
+# if guess == secretNumber:
+#   print(f"Good job {name}! You guessed your number in {guessTaken} guesses")
+# else:
+#   print(f"Nope. The number I was thinking of was {secretNumber}")
+  
+  
+age = 10 
+  
+while age < 20:
+    print("I am running" + str(age))
+    age += 1
