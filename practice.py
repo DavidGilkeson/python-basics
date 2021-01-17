aaf1e0aa45dee872398909337f5d6a65d9537f72
David Gilkeson <davidgilkeson8@gmail.com>
2021-01-17 21:21:53 +0000

    Practice updated

--- practice.py
+++ practice.py
@@ -76,15 +76,36 @@
 #             temp_list.append(list1[i-1][j-1]+list1[i-1][j]) 
 #     list1.append(temp_list)
 # print(list1)
-import pprint
+#  
 
-message = "It was a bright cold day in April and the clocks were striking thirteen"
-count = {}
+# a = 505
+# b = 300
+# c = 210
 
-for char in message:
-    count.setdefault(char, 0)
-    count[char] = count[char] + 1
+# if a > b and a > c:
+#     print(a)
+# elif b > c:
+#     print(b)
+# else:
+#     print(c)
+
+# Fizz Buzz
+
+# Write a short program that prints each number from 1 to 100 on a new line. 
+
+# For each multiple of 3, print "Fizz" instead of the number. 
+
+# For each multiple of 5, print "Buzz" instead of the number. 
+
+# For numbers which are multiples of both 3 and 5, print "FizzBuzz" instead of the number.
     
-pprint.pprint(count)
-
+for i in range(1,101):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")  
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
 
