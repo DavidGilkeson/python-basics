39a6769629917c022a299e54ebebe1cbc4c7847a
David Gilkeson <davidgilkeson8@gmail.com>
2021-01-19 01:47:50 +0000

    tuples updated

--- tuples.py
+++ tuples.py
@@ -69,7 +69,7 @@ tuple3 = (True, False, False)
 # A tuple with strings, integers and boolean values:
 
 tuple1 = ("abc", 34, True, 40, "male")
-type()
+# type()
 
 # From Python's perspective, tuples are defined as objects with the data type 'tuple':
 
@@ -87,3 +87,124 @@ print(type(mytuple))
 
 thistuple = tuple(("apple", "banana", "cherry")) # note the double round-brackets
 print(thistuple)
+
+# Access Tuple Items
+# You can access tuple items by referring to the index number, inside square brackets:
+
+
+# Print the second item in the tuple:
+
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[1])
+
+# Negative Indexing
+# Negative indexing means start from the end.
+
+# -1 refers to the last item, -2 refers to the second last item etc.
+
+# Print the last item of the tuple:
+
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[-1])
+
+# Range of Indexes
+# You can specify a range of indexes by specifying where to start and where to end the range.
+
+# When specifying a range, the return value will be a new tuple with the specified items.
+
+# Return the third, fourth, and fifth item:
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:5])
+
+# By leaving out the start value, the range will start at the first item:
+
+# This example returns the items from the beginning to, but NOT included, "kiwi":
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[:4])
+
+# By leaving out the end value, the range will go on to the end of the list:
+
+# This example returns the items from "cherry" and to the end:
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:])
+
+# Range of Negative Indexes
+# Specify negative indexes if you want to start the search from the end of the tuple:
+
+# This example returns the items from index -4 (included) to index -1 (excluded)
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[-4:-1])
+
+# Check if Item Exists
+# To determine if a specified item is present in a tuple use the in keyword:
+
+# Check if "apple" is present in the tuple:
+
+thistuple = ("apple", "banana", "cherry")
+if "apple" in thistuple:
+  print("Yes, 'apple' is in the fruits tuple")
+  
+  
+# Update Tuples
+# Tuples are unchangeable, meaing that you cannot change, add, or remove items once the tuple is created.
+
+# But there are some workarounds.
+
+# Change Tuple Values
+# Once a tuple is created, you cannot change its values. Tuples are unchangeable, or immutable as it also is called.
+
+# But there is a workaround. You can convert the tuple into a list, change the list, and convert the list back into a tuple.
+
+# Convert the tuple into a list to be able to change it:
+
+x = ("apple", "banana", "cherry")
+y = list(x)
+y[1] = "kiwi"
+x = tuple(y)
+
+print(x)
+
+
+# Add Items
+# Once a tuple is created, you cannot add items to it.
+
+# Example
+# You cannot add items to a tuple:
+
+# thistuple = ("apple", "banana", "cherry")
+# thistuple.append("orange") # This will raise an error
+# print(thistuple)
+
+# Just like the workaround for changing a tuple, you can convert it into a list, add your item(s), and convert it back into a tuple.
+
+# Convert the tuple into a list, add "orange", and convert it back into a tuple:
+
+thistuple = ("apple", "banana", "cherry")
+y = list(thistuple)
+y.append("orange")
+thistuple = tuple(y)
+
+
+# Remove Items
+# Note: You cannot remove items in a tuple.
+
+# Tuples are unchangeable, so you cannot remove items from it, but you can use the same workaround as we used for changing and adding tuple items:
+
+# Convert the tuple into a list, remove "apple", and convert it back into a tuple:
+
+thistuple = ("apple", "banana", "cherry")
+y = list(thistuple)
+y.remove("apple")
+thistuple = tuple(y)
+
+# Or you can delete the tuple completely:
+
+# The del keyword can delete the tuple completely:
+
+thistuple = ("apple", "banana", "cherry")
+del thistuple
+# print(thistuple) #this will raise an error because the tuple no longer exists
