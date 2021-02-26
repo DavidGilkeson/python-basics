045f1f8ab0f4030f51e78d64f522bf52725916fc
David Gilkeson <davidgilkeson8@gmail.com>
2021-02-26 02:00:30 +0000

    pandas updated

--- Pandas/intro.py
+++ Pandas/intro.py
@@ -26,4 +26,58 @@
 # Pandas are also able to delete rows that are not relevant, or contains wrong values, like empty or NULL values. This is called cleaning the data.
 
 # Where is the Pandas Codebase?
-# The source code for Pandas is located at this github repository https://github.com/pandas-dev/pandas
+# The source code for Pandas is located at this github repository https://github.com/pandas-dev/pandasPandas Getting Started
+
+
+# Installation of Pandas
+# If you have Python and PIP already installed on a system, then installation of Pandas is very easy.
+
+# Install it using this command:
+
+# C:\Users\Your Name>pip install pandas
+# If this command fails, then use a python distribution that already has Pandas installed like, Anaconda, Spyder etc.
+
+
+# Import Pandas
+# Once Pandas is installed, import it in your applications by adding the import keyword:
+
+# import pandas
+# Now Pandas is imported and ready to use.
+
+import pandas
+
+mydataset = {
+  'cars': ["BMW", "Volvo", "Ford"],
+  'passings': [3, 7, 2]
+}
+
+myvar = pandas.DataFrame(mydataset)
+
+print(myvar)
+
+# Pandas as pd
+# Pandas is usually imported under the pd alias.
+
+# alias: In Python alias are an alternate name for referring to the same thing.
+
+# Create an alias with the as keyword while importing:
+
+# import pandas as pd
+# Now the Pandas package can be referred to as pd instead of pandas.
+
+import pandas as pd
+
+mydataset = {
+  'cars': ["BMW", "Volvo", "Ford", "Nissan"],
+  'passings': [3, 7, 2, 6]
+}
+
+myvar = pd.DataFrame(mydataset)
+
+print(myvar)
+
+# Checking Pandas Version
+# The version string is stored under __version__ attribute.
+
+
+print(pd.__version__)
